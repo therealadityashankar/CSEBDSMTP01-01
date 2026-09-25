cdaa972404635d14f93b1d619e89677196c7a3e8
River <me@river.berlin>
2026-09-25 22:11:41 +0200

    added a test with more than 500 characters to test out how that works

--- test_client.py
+++ test_client.py
@@ -1,4 +1,4 @@
-import json, urllib.request
+import json, urllib.error, urllib.request
 
 url = "http://localhost:8000"
 sentences = json.load(open("test_sentences.json"))
@@ -9,5 +9,9 @@ for s in sentences:
         data=json.dumps({"text": s}).encode(),
         headers={"Content-Type": "application/json"},
     )
-    res = json.loads(urllib.request.urlopen(req).read())
-    print(f"{res['original']}\n=> {res['corrected']}\n")
+    try:
+        res = json.loads(urllib.request.urlopen(req).read())
+        print(f"{res['original']}\n=> {res['corrected']}\n")
+    except urllib.error.HTTPError as e:
+        body = json.loads(e.read())
+        print(f"{s}\n=> HTTP {e.code}: {body.get('detail')}\n")
